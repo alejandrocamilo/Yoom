b4ac6dbbbdcf7dcc21c9e8a76e48a4d565b7fe16
alejandrocamilo <alejandrocamilojavier5@gmail.com>
2025-05-30 01:15:24 -0400

    implementing Custom Hoook

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+
+import { useGetCalls } from "@/hooks/useGetCalls";
+import { CallRecording } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const CallList = ({ type }: { type: "upcoming" | "ended" | "recordings" }) => {
-  return <div>CallList</div>;
+  const { endedCalls, upcomingCalls, callRecordings, isLoading } =
+    useGetCalls();
+
+  const router = useRouter();
+
+  const [recordings, setRecordings] = useState<CallRecording[]>([]);
+
+  const getCalls = () => {
+    switch (type) {
+      case "ended":
+        return endedCalls;
+      case "upcoming":
+        return upcomingCalls;
+      case "recordings":
+        return recordings;
+      default:
+        return [];
+    }
+  };
+
+  const getNoCallsMessage = () => {
+    switch (type) {
+      case "ended":
+        return "No Previous Calls";
+      case "upcoming":
+        return "No Upcoming Calls";
+      case "recordings":
+        return "No Recordings";
+      default:
+        return "";
+    }
+  };
+  return <div className="grid grid-cols-1 gap-5 xl:grid-cols-2 "></div>;
 };
 
 export default CallList;
